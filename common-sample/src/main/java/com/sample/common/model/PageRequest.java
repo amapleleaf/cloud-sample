a6caa857edb26e53a27c9ffb261b72e989f73264
leixuefeng <leixuefeng-2005@163.com>
2021-04-21 17:52:01 +0800

    spring cloud oauth 版本升级spring cloud oauth 版本升级

--- common-sample/src/main/java/com/sample/common/model/PageRequest.java
+++ common-sample/src/main/java/com/sample/common/model/PageRequest.java
@@ -1,7 +1,9 @@
 package com.sample.common.model;
 
 public class PageRequest {
+    //查询页，默认值1
     private Integer pageNum;
+    //每页大小，默认值10
     private Integer size;
 
     public PageRequest(Integer pageNum, Integer size) {
